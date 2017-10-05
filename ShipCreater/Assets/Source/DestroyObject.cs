b318a9ea8823793cf9dba9843c4fd4578a418a65
杉山　涼 <sugisugirr@sugiyama-no-MacBook-Airlocal.local>
2017-10-05 10:08:33 +0900

    ツールのアップデート

---
+++ ShipCreater/Assets/Source/DestroyObject.cs
@@ -0,0 +1,17 @@
+﻿/* **********************************************
+ * CopyRight ©︎ Ryo Sugiyama all write reserved
+ * **********************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+	public void Update() {
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			Debug.Log ("Destroy");
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
